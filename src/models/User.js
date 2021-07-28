728e0fa67ceab5a098fd5e3fffe3be63e6852405
Lucas Matheus <lucasmatheus2021@gmail.com>
2021-07-28 02:42:18 -0300

    feat: set up Schema usage
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,19 @@
+const { Schema } = require('pyniedb');
 const pyniedb = require('../config/database');
 
-const User = pyniedb.Model('User');
+const UserSchema = new Schema({
+	name: {
+		type: 'string',
+	},
+	email: {
+		type: 'string',
+	},
+	active: {
+		type: 'boolean',
+		default: false,
+	}
+});
+
+const User = pyniedb.Model('User', UserSchema);
 
 module.exports = User;
